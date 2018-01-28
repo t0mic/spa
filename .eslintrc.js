39d105c7b67db23438a71a7a6c805dbb61a1cf48
Marko Tomic <markotomic.96@hotmail.com>
2018-01-28 19:47:12 +0100

    eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,17 @@ module.exports = {
   plugins: [
     'vue'
   ],
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': "module",
+    'ecmaFeatures': {
+      'jsx': true
+    }
+  },
   // add your custom rules here
   rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
